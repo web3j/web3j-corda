386ae08867d173e03786e043988661061e1fd6eb
Xavier <xavier@blk.io>
2019-07-31 12:09:07 +0100

    Convert main method to JUnit test

--- common/src/test/kotlin/org/web3j/examples/obligation/ObligationTest.kt
+++ common/src/test/kotlin/org/web3j/examples/obligation/ObligationTest.kt
@@ -1,23 +1,28 @@
 package org.web3j.examples.obligation
 
+import org.junit.jupiter.api.Test
 import org.web3j.corda.Party
 import org.web3j.corda.SignedTransaction
 import org.web3j.corda.protocol.Corda
 import org.web3j.corda.protocol.CordaService
 
-fun main() {
-    val party = Party("", "")
-    val service = CordaService("http://localhost:9000/")
+class ObligationTest {
 
-    val corda = Corda.build(service)
-    corda.network.nodes.forEach { println(it) }
+    @Test
+    fun `issue obligation`() {
+        val party = Party("", "")
+        val service = CordaService("http://localhost:9000/")
 
-    // 1. Normal version, not type safe
-    val signedTx = corda
-        .corDappById("obligation-cordapp")
-        .flowById("issue-obligation")
-        .start(party) as SignedTransaction
+        val corda = Corda.build(service)
+        corda.network.nodes.forEach { println(it) }
 
-    // 2. web3j generated version, 100% type safe
-    Obligation.Issue.Builder().build(corda).start(party)
+        // 1. Normal version, not type safe
+        val signedTx = corda
+            .corDappById("obligation-cordapp")
+            .flowById("issue-obligation")
+            .start(party) as SignedTransaction
+
+        // 2. web3j generated version, 100% type safe
+        Obligation.Issue.Builder().build(corda).start(party)
+    }
 }
