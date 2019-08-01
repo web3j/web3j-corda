7330fa8603c6f7a5d64ea9b9ffeebae7f270491f
Xavier <xavier@blk.io>
2019-08-01 09:15:46 +0100

    Make type private

--- common/src/main/kotlin/org/web3j/corda/contracts/ContractBuilder.kt
+++ common/src/main/kotlin/org/web3j/corda/contracts/ContractBuilder.kt
@@ -3,7 +3,7 @@ package org.web3j.corda.contracts
 import org.glassfish.jersey.client.proxy.WebResourceFactory
 import org.web3j.corda.protocol.Corda
 
-abstract class ContractBuilder<T> protected constructor(val type: Class<T>) {
+abstract class ContractBuilder<T> protected constructor(private val type: Class<T>) {
 
     fun build(corda: Corda): T {
         val target = corda.service.client.target(corda.service.uri)
