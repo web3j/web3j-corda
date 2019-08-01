3427150b2aad6570f7adc211a95a6741a503ed87
Xavier <xavier@blk.io>
2019-08-01 09:16:02 +0100

    Add config test methods

--- common/src/test/kotlin/org/web3j/examples/obligation/ObligationKotlinTest.kt
+++ common/src/test/kotlin/org/web3j/examples/obligation/ObligationKotlinTest.kt
@@ -1,7 +1,9 @@
 package org.web3j.examples.obligation
 
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.web3j.corda.api.toJson
+import org.web3j.corda.SignedTransaction
 import org.web3j.corda.protocol.Corda
 import org.web3j.corda.protocol.CordaService
 
@@ -9,21 +11,37 @@ class ObligationKotlinTest {
 
     @Test
     fun `issue obligation`() {
-        val service = CordaService("http://localhost:9000/")
-
-        val corda = Corda.build(service)
-        corda.getNetwork().getAllNodes().forEach { println(it.toJson()) }
+        val party = corda.getNetwork().getAllNodes()[2].legalIdentities[0]
 
         // 1. Normal version, not type safe
-//        val signedTx = corda
-//            .getCorDappById("obligation-cordapp")
-//            .getFlowById("issue-obligation")
-//            .start(party) as SignedTransaction
+        val signedTx = corda
+            .getCorDappById("obligation-cordapp")
+            .getFlowById("issue-obligation")
+            .start(party) as SignedTransaction
 
         // 2. web3j generated version, 100% type safe
-        val party = corda.getNetwork().getAllNodes()[2].legalIdentities[0]
         Obligation.build(corda).getIssue().start("$1", party.name, false)
+    }
 
-        service.close()
+    companion object {
+
+        @JvmStatic
+        private lateinit var corda: Corda
+
+        @JvmStatic
+        private lateinit var service: CordaService
+
+        @BeforeAll
+        @JvmStatic
+        @Throws(Exception::class)
+        fun setUpClass() {
+            service = CordaService("http://localhost:9000/")
+            corda = Corda.build(service)
+        }
+
+        @AfterAll
+        internal fun tearDownClass() {
+            service.close()
+        }
     }
 }
