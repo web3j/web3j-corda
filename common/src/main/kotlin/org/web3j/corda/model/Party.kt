09d672fa178b032273a2eff76a1be74e1d9bed76
Puneetha <puneetha17@gmail.com>
2019-08-02 13:27:26 +0100

    Update SignedTx model.

--- common/src/main/kotlin/org/web3j/corda/model/Party.kt
+++ common/src/main/kotlin/org/web3j/corda/model/Party.kt
@@ -4,6 +4,6 @@ import org.web3j.corda.validation.X500Name
 
 data class Party(
     @field:X500Name
-    val name: CordaX500Name,
+    val name: CordaX500Name?,
     val owningKey: PublicKey
 )
--- common/src/main/kotlin/org/web3j/corda/model/SignedTransaction.kt
+++ common/src/main/kotlin/org/web3j/corda/model/SignedTransaction.kt
@@ -1,20 +1,73 @@
 package org.web3j.corda.model
 
 data class SignedTransaction(
-    val txBits: ByteArray
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+    val signatures: List<String>,
+    val references: List<String>,
+    val networkParametersHash: String,
+    val coreTransaction: CoreTransaction,
+    val notaryChangeTransaction: Boolean,
+    val missingSigners: List<String>
+)
 
-        other as SignedTransaction
+data class CoreTransaction(
+    val componentGroups: List<ComponentGroup>,
+    val privacySalt: String,
+    val attachments: List<String>,
+    val inputs: List<String>,
+    val references: List<String>,
+    val outputs: List<Output?>,
+    val commands: List<Commands>,
+    val notary: Party,
+    val timeWindow: TimeWindow,
+    val networkParametersHash: String,
+    val id: String,
+    val requiredSigningKeys: List<String>,
+    val `groupHashes$core`: List<String>,
+    val `groupsMerkleRoots$core`: Map<String, String>,
+    val `availableComponentNonces$core`: Map<String, List<String>>,
+    val `availableComponentHashes$core`: Map<String, List<String>>,
+    val availableComponentGroups: List<List<Any?>?>
+)
+data class TimeWindow(
+    val fromTime: String,
+    val untilTime: String,
+    val midpoint: String,
+    val length: String
+)
+data class Commands(
+    val value: Value?,
+    val signers: List<String>
+)
+data class Value(val value: String?)
+data class ComponentGroup(
+    val groupIndex: Int,
+    val components: List<String>
+)
 
-        if (!txBits.contentEquals(other.txBits)) return false
+data class Output(
+    val data: Data?,
+    val contract: String?,
+    val notary: Party?,
+    val encumbrance: String?,
+    val constraint: Constraint?
+)
+data class Constraint(val attachmentId: String)
+data class Data(
+    val amount: Money,
+    val lender: Party,
+    val borrower: Party,
+    val paid: Money,
+    val linearId: LinearId,
+    val participants: List<Party>
+)
 
-        return true
-    }
+data class LinearId(
+    val externalId: String?,
+    val id: String
+)
 
-    override fun hashCode(): Int {
-        return txBits.contentHashCode()
-    }
-}
+data class Money(
+    val quantity: Int,
+    val displayTokenSize: Float,
+    val token: String
+)
