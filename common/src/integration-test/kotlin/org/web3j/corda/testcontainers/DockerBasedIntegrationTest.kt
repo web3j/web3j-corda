d47cc855b75e464372955913d46ab4dbc58a17d3
Xavier <xavier@blk.io>
2019-09-03 10:49:11 +0100

    Move methods to companion object

--- common/src/integration-test/kotlin/org/web3j/corda/testcontainers/DockerBasedIntegrationTest.kt
+++ common/src/integration-test/kotlin/org/web3j/corda/testcontainers/DockerBasedIntegrationTest.kt
@@ -38,9 +38,6 @@ import java.util.concurrent.CountDownLatch
 @Testcontainers
 open class DockerBasedIntegrationTest {
 
-    @TempDir
-    lateinit var nodes: File
-
     @Test
     fun `test to setup docker containers`() {
         val notary = createNodeContainer("Notary", "London", "GB", 10005, 10006, 10007, true)
@@ -53,10 +50,10 @@ open class DockerBasedIntegrationTest {
         partyB.start()
 
         Braid().withPort(9000)
-                .withUserName("user1")
-                .withPassword("test")
-                .withNodeAddress("localhost:${partyA.getMappedPort(10009)}")
-                .startServer()
+            .withUserName("user1")
+            .withPassword("test")
+            .withNodeAddress("localhost:${partyA.getMappedPort(10009)}")
+            .startServer()
 
         CountDownLatch(1).await()
         notary.stop()
@@ -64,127 +61,22 @@ open class DockerBasedIntegrationTest {
         partyB.stop()
     }
 
-    protected fun createNodeContainer(
-        name: String,
-        location: String,
-        country: String,
-        p2pPort: Int,
-        rpcPort: Int,
-        adminPort: Int,
-        isNotary: Boolean
-    ): KGenericContainer {
-        val nodeDir = File(nodes, name).apply { mkdir() }
-        createNodeConfFiles(
-            name,
-            location,
-            country,
-            p2pPort,
-            rpcPort,
-            adminPort,
-            nodeDir.resolve("node.conf"),
-            isNotary
-        )
-        getCertificate(nodeDir)
-        val node = KGenericContainer(CORDA_ZULU_IMAGE)
-            .withNetwork(network)
-            .withExposedPorts(p2pPort, rpcPort, adminPort)
-            .withFileSystemBind(PREFIX + nodeDir.absolutePath, "/etc/corda", READ_WRITE)
-            .withFileSystemBind(
-                PREFIX + nodeDir.resolve("certificates").absolutePath,
-                "/opt/corda/certificates",
-                READ_WRITE
-            )
-            .withEnv("NETWORKMAP_URL", NETWORK_MAP_URL)
-            .withEnv("DOORMAN_URL", NETWORK_MAP_URL)
-            .withEnv("NETWORK_TRUST_PASSWORD", "trustpass")
-            .withEnv("MY_PUBLIC_ADDRESS", "http://localhost:$p2pPort")
-            .withCommand("config-generator --generic")
-            .withStartupTimeout(timeOut)
-            .withCreateContainerCmdModifier {
-                it.withHostName(name.toLowerCase())
-                it.withName(name.toLowerCase())
-            }
-
-        if (isNotary) {
-            node.start()
-
-            val nodeInfo = extractNotaryNodeInfo(node, nodeDir)
-            node.stop()
-
-            updateNotaryInNetworkMap(nodeDir.resolve(nodeInfo).absolutePath)
-        } else {
-            node.withClasspathResourceMapping("cordapps", "/opt/corda/cordapps", READ_WRITE)
-        }
-
-        return node
-    }
-
-    private fun createNodeConfFiles(
-        name: String,
-        location: String,
-        country: String,
-        p2pPort: Int,
-        rpcPort: Int,
-        adminPort: Int,
-        file: File,
-        isNotary: Boolean
-    ) {
-        Mustache.compiler()
-            .compile("nodeConf.mustache")
-            .execute(
-                hashMapOf(
-                    "name" to name,
-                    "isNotary" to isNotary,
-                    "location" to location,
-                    "country" to country,
-                    "p2pPort" to p2pPort,
-                    "rpcPort" to rpcPort,
-                    "adminPort" to adminPort,
-                    "networkMapUrl" to NETWORK_MAP_URL
-                ),
-                PrintWriter(OutputStreamWriter(FileOutputStream(file)))
-            )
-    }
-
-    private fun getCertificate(node: File) {
-        val certificateFolder = File(node, "certificates").apply { mkdir() }
-        val certificateFile = certificateFolder.resolve("network-root-truststore.jks")
-
-        NetworkMap.build(CordaService("http://localhost:${NETWORK_MAP.getMappedPort(8080)}")).apply {
-            Files.write(certificateFile.toPath(), networkMap.truststore)
-        }
-    }
-
-    private fun extractNotaryNodeInfo(notary: KGenericContainer, notaryNode: File): String {
-        var nodeinfo = notary.execInContainer("find", ".", "-maxdepth", "1", "-name", "nodeInfo*").stdout
-        nodeinfo = nodeinfo.substring(2, nodeinfo.length - 1) // remove the ending newline character
-
-        notary.copyFileFromContainer(
-            "/opt/corda/$nodeinfo",
-            notaryNode.resolve(nodeinfo).absolutePath
-        )
-        notary.execInContainer("rm", "network-parameters")
-        return nodeinfo
-    }
-
-    private fun updateNotaryInNetworkMap(nodeInfoPath: String) {
-        var networkMapApi = NetworkMap.build(CordaService("http://localhost:${NETWORK_MAP.getMappedPort(8080)}"))
-        val loginRequest = LoginRequest("sa", "admin")
-
-        val token = networkMapApi.admin.login(loginRequest)
-        networkMapApi = NetworkMap.build(CordaService("http://localhost:${NETWORK_MAP.getMappedPort(8080)}"), token)
-        networkMapApi.admin.notaries.create(NotaryType.NON_VALIDATING, Files.readAllBytes(Paths.get(nodeInfoPath)))
-    }
-
     companion object {
 
-        val PREFIX = if (System.getProperty("os.name").contains("Mac", true)) "/private" else ""
+        private val PREFIX = if (System.getProperty("os.name").contains("Mac", true)) {
+            "/private"
+        } else {
+            ""
+        }
 
-        const val NETWORK_MAP_ALIAS = "networkmap"
-        const val NETWORK_MAP_URL = "http://${NETWORK_MAP_ALIAS}:8080"
+        private const val NETWORK_MAP_ALIAS = "networkmap"
+        private const val NETWORK_MAP_URL = "http://${NETWORK_MAP_ALIAS}:8080"
 
-        const val NETWORK_MAP_IMAGE = "cordite/network-map:v0.4.5"
-        const val CORDA_ZULU_IMAGE = "corda/corda-zulu-4.1:latest"
+        private const val NETWORK_MAP_IMAGE = "cordite/network-map:v0.4.5"
+        private const val CORDA_ZULU_IMAGE = "corda/corda-zulu-4.1:latest"
+
+        @TempDir
+        private lateinit var nodes: File
 
         private val network: Network = Network.newNetwork()
         private val timeOut: Duration = Duration.ofMinutes(2)
@@ -200,6 +92,122 @@ open class DockerBasedIntegrationTest {
             .withNetworkAliases(NETWORK_MAP_ALIAS)
             .withEnv(mapOf(Pair("NMS_STORAGE_TYPE", "file")))
             .waitingFor(Wait.forHttp("").forPort(8080))
+
+        @JvmStatic
+        protected fun createNodeContainer(
+            name: String,
+            location: String,
+            country: String,
+            p2pPort: Int,
+            rpcPort: Int,
+            adminPort: Int,
+            isNotary: Boolean
+        ): KGenericContainer {
+            val nodeDir = File(nodes, name).apply { mkdir() }
+            createNodeConfFiles(
+                name,
+                location,
+                country,
+                p2pPort,
+                rpcPort,
+                adminPort,
+                nodeDir.resolve("node.conf"),
+                isNotary
+            )
+            getCertificate(nodeDir)
+            val node = KGenericContainer(CORDA_ZULU_IMAGE)
+                .withNetwork(network)
+                .withExposedPorts(p2pPort, rpcPort, adminPort)
+                .withFileSystemBind(PREFIX + nodeDir.absolutePath, "/etc/corda", READ_WRITE)
+                .withFileSystemBind(
+                    PREFIX + nodeDir.resolve("certificates").absolutePath,
+                    "/opt/corda/certificates",
+                    READ_WRITE
+                )
+                .withEnv("NETWORKMAP_URL", NETWORK_MAP_URL)
+                .withEnv("DOORMAN_URL", NETWORK_MAP_URL)
+                .withEnv("NETWORK_TRUST_PASSWORD", "trustpass")
+                .withEnv("MY_PUBLIC_ADDRESS", "http://localhost:$p2pPort")
+                .withCommand("config-generator --generic")
+                .withStartupTimeout(timeOut)
+                .withCreateContainerCmdModifier {
+                    it.withHostName(name.toLowerCase())
+                    it.withName(name.toLowerCase())
+                }
+
+            if (isNotary) {
+                node.start()
+
+                val nodeInfo = extractNotaryNodeInfo(node, nodeDir)
+                node.stop()
+
+                updateNotaryInNetworkMap(nodeDir.resolve(nodeInfo).absolutePath)
+            } else {
+                node.withClasspathResourceMapping("cordapps", "/opt/corda/cordapps", READ_WRITE)
+            }
+
+            return node
+        }
+
+        private fun createNodeConfFiles(
+            name: String,
+            location: String,
+            country: String,
+            p2pPort: Int,
+            rpcPort: Int,
+            adminPort: Int,
+            file: File,
+            isNotary: Boolean
+        ) {
+            Mustache.compiler()
+                .compile("nodeConf.mustache")
+                .execute(
+                    hashMapOf(
+                        "name" to name,
+                        "isNotary" to isNotary,
+                        "location" to location,
+                        "country" to country,
+                        "p2pPort" to p2pPort,
+                        "rpcPort" to rpcPort,
+                        "adminPort" to adminPort,
+                        "networkMapUrl" to NETWORK_MAP_URL
+                    ),
+                    PrintWriter(OutputStreamWriter(FileOutputStream(file)))
+                )
+        }
+
+        private fun getCertificate(node: File) {
+            val certificateFolder = File(node, "certificates").apply { mkdir() }
+            val certificateFile = certificateFolder.resolve("network-root-truststore.jks")
+            val networkMapUrl = "http://localhost:${NETWORK_MAP.getMappedPort(8080)}"
+
+            NetworkMap.build(CordaService(networkMapUrl)).apply {
+                Files.write(certificateFile.toPath(), networkMap.truststore)
+            }
+        }
+
+        private fun extractNotaryNodeInfo(notary: KGenericContainer, notaryNode: File): String {
+            var nodeInfo = notary.execInContainer("find", ".", "-maxdepth", "1", "-name", "nodeInfo*").stdout
+            nodeInfo = nodeInfo.substring(2, nodeInfo.length - 1) // remove the ending newline character
+
+            notary.copyFileFromContainer(
+                "/opt/corda/$nodeInfo",
+                notaryNode.resolve(nodeInfo).absolutePath
+            )
+            notary.execInContainer("rm", "network-parameters")
+            return nodeInfo
+        }
+
+        private fun updateNotaryInNetworkMap(nodeInfoPath: String) {
+            val networkMapUrl = "http://localhost:${NETWORK_MAP.getMappedPort(8080)}"
+            var networkMapApi = NetworkMap.build(CordaService(networkMapUrl))
+
+            val loginRequest = LoginRequest("sa", "admin")
+            val token = networkMapApi.admin.login(loginRequest)
+
+            networkMapApi = NetworkMap.build(CordaService(networkMapUrl), token)
+            networkMapApi.admin.notaries.create(NotaryType.NON_VALIDATING, Files.readAllBytes(Paths.get(nodeInfoPath)))
+        }
 
         class KGenericContainer(imageName: String) : GenericContainer<KGenericContainer>(imageName)
     }
