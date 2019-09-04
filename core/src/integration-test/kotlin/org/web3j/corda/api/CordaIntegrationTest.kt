8d0db451b5fd18656bcc230a3f59614e24b41c86
Xavier <xavier@blk.io>
2019-09-04 14:48:34 +0100

    Add assertions using data classes

--- core/src/integration-test/kotlin/org/web3j/corda/api/CordaIntegrationTest.kt
+++ core/src/integration-test/kotlin/org/web3j/corda/api/CordaIntegrationTest.kt
@@ -14,19 +14,22 @@ package org.web3j.corda.api
 
 import assertk.assertThat
 import assertk.assertions.hasSize
+import assertk.assertions.isDataClassEqualTo
 import assertk.assertions.isEmpty
-import assertk.assertions.isEqualTo
 import io.bluebank.braid.server.Braid
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.testcontainers.junit.jupiter.Testcontainers
+import org.web3j.corda.model.NetworkHostAndPort
+import org.web3j.corda.model.Party
+import org.web3j.corda.model.SimpleNodeInfo
 import org.web3j.corda.protocol.Corda
 import org.web3j.corda.protocol.CordaService
 import org.web3j.corda.testcontainers.DockerBasedIntegrationTest
 
 /**
- * TODO Implement tests.
+ * TODO Implement tests for all routes and error cases.
  */
 @Testcontainers
 class CordaIntegrationTest : DockerBasedIntegrationTest() {
@@ -49,24 +52,24 @@ class CordaIntegrationTest : DockerBasedIntegrationTest() {
 
         corda.network.nodes.self.apply {
             assertThat(legalIdentities).hasSize(1)
-            assertThat(legalIdentities.first().name).isEqualTo("O=Notary, L=London, C=GB")
+            assertThat(legalIdentities.first()).isDataClassEqualTo(party)
         }
 
         corda.network.nodes.findAll().apply {
-            assertThat(this).isEmpty()
+            assertThat(this).isDataClassEqualTo(notary)
         }
 
-        corda.network.nodes.findByX500Name("").apply {
-            assertThat(this).isEmpty()
+        corda.network.nodes.findByX500Name(party.name!!).apply {
+            assertThat(this).isDataClassEqualTo(notary)
         }
 
-        corda.network.nodes.findByHostAndPort("").apply {
-            assertThat(this).isEmpty()
+        corda.network.nodes.findByHostAndPort("localhost:10005").apply {
+            assertThat(this).isDataClassEqualTo(notary)
         }
 
         corda.network.notaries.findAll().apply {
             assertThat(this).hasSize(1)
-            assertThat(first().name).isEqualTo("Notary")
+            assertThat(first()).isDataClassEqualTo(party)
         }
     }
 
@@ -74,10 +77,20 @@ class CordaIntegrationTest : DockerBasedIntegrationTest() {
 
         private lateinit var corda: Corda
 
+        private val party = Party(
+            name = "O=Notary, L=London, C=GB",
+            owningKey = "GfHq2tTVk9z4eXgyQKmUDm9Hyk7bB8yh6bMXvhmaikGFxUDrHhFnJhNiqN5Z"
+        )
+
+        private val notary = SimpleNodeInfo(
+            addresses = listOf(NetworkHostAndPort("localhost", 10005)),
+            legalIdentities = listOf(party)
+        )
+
         @BeforeAll
         @JvmStatic
         fun setUp() {
-            val notary = createNodeContainer(
+            val container = createNodeContainer(
                 "Notary",
                 "London",
                 "GB",
@@ -92,7 +105,7 @@ class CordaIntegrationTest : DockerBasedIntegrationTest() {
             Braid().withPort(9000)
                 .withUserName("user1")
                 .withPassword("test")
-                .withNodeAddress("localhost:${notary.getMappedPort(10006)}")
+                .withNodeAddress("localhost:${container.getMappedPort(10006)}")
                 .startServer()
 
             corda = Corda.build(CordaService("http://localhost:9000"))
