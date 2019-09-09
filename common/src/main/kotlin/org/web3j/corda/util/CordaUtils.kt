a7d14e25935fb89160ba0c1771f973a919c91f09
Xavier <xavier@blk.io>
2019-09-09 16:22:56 +0100

    Formatting

--- common/src/main/kotlin/org/web3j/corda/util/CordaUtils.kt
+++ common/src/main/kotlin/org/web3j/corda/util/CordaUtils.kt
@@ -11,6 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 @file:JvmName("CordaUtils")
+
 package org.web3j.corda.util
 
 import com.fasterxml.jackson.databind.ObjectMapper
