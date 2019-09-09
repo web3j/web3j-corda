eef270c2f7c20f56c784e133b60b2a7ec2830bad
Puneetha <puneetha17@gmail.com>
2019-09-09 10:05:18 +0100

    Add type boundary in LifeCycle

--- core/src/main/kotlin/org/web3j/corda/dapps/LifeCycle.kt
+++ core/src/main/kotlin/org/web3j/corda/dapps/LifeCycle.kt
@@ -12,10 +12,11 @@
  */
 package org.web3j.corda.dapps
 
+import org.web3j.corda.api.CorDapp
 import org.web3j.corda.protocol.Corda
 import java.io.File
 
-interface LifeCycle<T> {
+interface LifeCycle<T: CorDapp> {
     fun load(corda: Corda): T
     fun deploy(corda: Corda, file: File): T
     fun upgrade(corda: Corda, file: File): T
