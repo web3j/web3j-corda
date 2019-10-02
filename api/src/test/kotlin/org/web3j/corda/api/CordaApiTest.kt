9b8af9b289e769b761d7dde29da1b55e4d19047f
Puneetha Karamsetty <puneetha@blk.io>
2019-10-02 14:36:03 +0000

    Add simple Wiremock unittest

---
+++ api/src/test/kotlin/org/web3j/corda/api/CordaApiTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 Web3 Labs LTD.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.web3j.corda.api
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.matching
+import com.github.tomakehurst.wiremock.client.WireMock.urlMatching
+import com.github.tomakehurst.wiremock.client.WireMock.verify
+import org.apache.http.client.methods.HttpGet
+import org.junit.jupiter.api.Test
+import javax.ws.rs.core.MediaType
+
+
+class CordaApiTest : WireMockInterface() {
+
+    @Test
+    fun `corda network test`() {
+
+        val request = HttpGet("http://localhost:8080/api/rest/network")
+        request.addHeader("Content-Type", MediaType.APPLICATION_JSON)
+
+        val httpResponse = httpClient.execute(request)
+
+        assertThat(httpResponse.statusLine.statusCode).isEqualTo(200)
+
+        verify(getRequestedFor(urlMatching("/api/rest/network"))
+            .withHeader("Content-Type", matching(MediaType.APPLICATION_JSON)))
+    }
+}
