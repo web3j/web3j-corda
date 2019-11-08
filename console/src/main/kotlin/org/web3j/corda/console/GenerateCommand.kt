1e32aebecd82e7a238ceb601304c2228174b5524
Puneetha <puneetha17@gmail.com>
2019-11-08 14:56:53 +0000

    Use shadow jar in the distribution zip and tar

--- console/src/main/kotlin/org/web3j/corda/console/GenerateCommand.kt
+++ console/src/main/kotlin/org/web3j/corda/console/GenerateCommand.kt
@@ -15,6 +15,7 @@ package org.web3j.corda.console
 import io.bluebank.braid.corda.server.BraidDocsMain
 import io.bluebank.braid.core.utils.toJarsClassLoader
 import io.bluebank.braid.core.utils.tryWithClassLoader
+import java.io.File
 import java.net.URL
 import java.nio.charset.StandardCharsets.UTF_8
 import kotlin.streams.toList
@@ -61,9 +62,15 @@ class GenerateCommand : BaseCommand() {
             fetchOpenApiDef()
         }.apply {
 
-            org.jetbrains.kotlin.konan.file.File(outputDir.absolutePath, "swagger.json").printWriter().use { out ->
-                out.println(this)
-            }
+            // Output the swagger file into clients folder
+            File(outputDir.absolutePath)
+                .apply {
+                    mkdirs()
+                    resolve("swagger.json")
+                        .printWriter().use { out ->
+                            out.println(this)
+                        }
+                }
 
             CorDappClientGenerator(
                 packageName,
