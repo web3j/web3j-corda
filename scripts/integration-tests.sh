5ea1cbdf14db34c6deb9d9dcedb186441d647624
Xavier Arias <xavier@web3labs.com>
2019-11-13 15:10:40 +0000

    Add shebang line

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DOCKER_HOST=unix:///var/run/docker.sock DOCKER_TLS_VERIFY= docker run -t --rm \
           -v "$HOME/.gradle":/root/.gradle/ \
           -v /var/run/docker.sock:/var/run/docker.sock \
