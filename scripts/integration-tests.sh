ba9bacd9fcdc199647db6a245c3c7e5446a86080
Xavier Arias <xavier@web3labs.com>
2019-11-18 16:47:11 +0000

    Add stack trace to Travis CI integration test build

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -6,4 +6,4 @@ DOCKER_HOST=unix:///var/run/docker.sock DOCKER_TLS_VERIFY= docker run -t --rm \
           -v "$(pwd)":"$(pwd)" \
           -w "$(pwd)" \
           openjdk:8-jdk-alpine \
-          ./gradlew --no-daemon spotlessCheck check integrationTest jacocoTestReport -Pversion="${TRAVIS_BRANCH#'release/'}"
+          ./gradlew --no-daemon --stacktrace spotlessCheck check integrationTest jacocoTestReport -Pversion="${TRAVIS_BRANCH#'release/'}"
