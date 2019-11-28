52a7491773b3d12c65d242888f4646e346d1e8d1
Xavier Arias <xavier@web3labs.com>
2019-11-28 12:16:14 +0000

    Spotless

--- console/src/main/kotlin/org/web3j/corda/console/GenerateCommand.kt
+++ console/src/main/kotlin/org/web3j/corda/console/GenerateCommand.kt
@@ -15,7 +15,6 @@ package org.web3j.corda.console
 import io.bluebank.braid.corda.server.BraidDocsMain
 import io.bluebank.braid.core.utils.toJarsClassLoader
 import io.bluebank.braid.core.utils.tryWithClassLoader
-import java.io.File
 import java.net.URL
 import java.nio.charset.StandardCharsets.UTF_8
 import kotlin.streams.toList
