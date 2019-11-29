2f635bb36655282d38c653248b62fe9bc0257c0c
Xavier Arias <xavier@web3labs.com>
2019-11-29 18:32:39 +0000

    prepare release 0.2.4

--- scripts/win_installer.ps1
+++ scripts/win_installer.ps1
@@ -2,7 +2,7 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues['*:ErrorAction']='Stop'
 
-$web3j_corda_version="0.2.3"
+$web3j_corda_version="0.2.4"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $ProgressPreference = 'SilentlyContinue'
