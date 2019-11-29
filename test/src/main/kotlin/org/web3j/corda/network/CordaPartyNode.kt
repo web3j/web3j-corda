70254bd1134e9d8a6f5f6bbe6c0627f8426c3204
Xavier Arias <xavier@web3labs.com>
2019-11-29 18:26:51 +0000

    Revert Braid to RC10 until classpath error is fixed

--- test/src/main/kotlin/org/web3j/corda/network/CordaPartyNode.kt
+++ test/src/main/kotlin/org/web3j/corda/network/CordaPartyNode.kt
@@ -104,7 +104,7 @@ class CordaPartyNode internal constructor(network: CordaNetwork) : CordaNode(net
     }
 
     companion object {
-        private const val BRAID_IMAGE = "cordite/braid:v4.1.2-RC11"
+        private const val BRAID_IMAGE = "cordite/braid:v4.1.2-RC10"
         private const val BRAID_ALIAS = "braid"
     }
 }
